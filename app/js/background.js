425e43af86b7e98092a8cefc4157113627aa5329
Tim Leland <timothyl88@gmail.com>
2015-08-07 00:42:30 -0400

    V2.7

--- app/js/background.js
+++ app/js/background.js
@@ -1,17 +1,21 @@
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (alarm.name == 'updateTemp') {
-        var coords = {};
-        coords.latitude = localStorage.getItem('latitude');
-        coords.longitude = localStorage.getItem('longitude');
-        if (coords.latitude && coords.longitude) {
-            getForecast(coords, true);
-        }
+        updateBadgeTemp();
     };
 });
 
-
+var updateBadgeTemp = function(){
+    var coords = {};
+    coords.latitude = localStorage.getItem('latitude');
+    coords.longitude = localStorage.getItem('longitude');
+    if (coords.latitude && coords.longitude) {
+        getForecast(coords);
+    }
+};
 
 window.onload = function() {
+    //Update on load
+    updateBadgeTemp();
     chrome.alarms.clear('updateTemp');
     chrome.alarms.create('updateTemp', {
         periodInMinutes: 60
--- app/js/popup.js
+++ app/js/popup.js
@@ -14,13 +14,25 @@ var weatherIcons = {
     'tornado': '<i class="wi wi-tornado"></i>'
 };
 
+var badgeIcons = {
+    'clear-day': 'wi-day-sunny.png',
+    'clear-night': 'wi-night-clear.png',
+    'rain': 'wi-showers.png',
+    'snow': 'wi-snow.png',
+    'sleet': 'wi-sleet.png',
+    'wind': 'wi-windy.png',
+    'fog': 'wi-fog.png',
+    'cloudy': 'wi-cloudy.png',
+    'partly-cloudy-day': 'wi-day-cloudy.png',
+    'partly-cloudy-night': 'wi-night-cloudy.png',
+    'hail': 'wi-hail.png',
+    'thunderstorm': 'wi-thunderstorm.png',
+    'tornado': 'wi-tornado.png'
+};
+
 var apikeys = [];
 
 var getWeatherRetry = 0;
-
-var roundTwoDecimals = function(number) {
-    return parseFloat(number).toFixed(2)
-};
 
 var getLocation = function() {
     var zipcode = localStorage.getItem('zipcode');
@@ -60,7 +72,7 @@ var processCoordinates = function(coords) {
     localStorage.setItem('latitude', coords.latitude);
     localStorage.setItem('longitude', coords.longitude);
     //Removed to reduce api calls
-    getForecast(coords, false);
+    getForecast(coords);
     getCityState(coords);
 };
 
@@ -77,47 +89,60 @@ var getCityState = function(coords) {
             if (ac.types.indexOf("locality") >= 0) city = ac.long_name;
             if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
         }
-        $('.current .location').text(city + (state ? ', ' + state : ''))
+        $('.current .location').html('<i class="fa fa-location-arrow"></i> ' + (city ? city : '') + (city && state ? ', ' : '') + (state ? state : ''));
     });
 };
 
-var getForecast = function(coords, fromBackground) {
+var getForecast = function(coords) {
     var randomApiKey = apikeys[Math.floor(Math.random() * apikeys.length)]
     var weatherUrl = 'https://api.forecast.io/forecast/' + randomApiKey + '/' + coords.latitude + ',' + coords.longitude;
 
     $.get(weatherUrl, function(data) {
         console.log(data);
-        if (!fromBackground) {
+        if ($('#popup').length > 0) {
+            $('.tooltip li, .temperature').tooltipster({
+                contentAsHTML: true,
+                animation: 'grow',
+                maxWidth: 150
+            });
             displayForcast(data.daily);
             displayCurrent(data.currently);
             displayHourly(data.hourly);
             displayAlerts(data.alerts);
+
+
         }
 
-        updateBadge(data.currently.temperature);
-    }).fail(function() {
-        _gaq.push(['_trackEvent', 'Key Failed', randomApiKey]);
-        if (getWeatherRetry < 3) {
-            getWeatherRetry++;
-            setTimeout(function() {
-                getLocation();
-            }, 1000);
-        } else {
-            _gaq.push(['_trackEvent', 'Failed Multiple Times', 'Last Key: ' + randomApiKey]);
-            alert('Something went wrong. Please try again.')
+        updateBadge(data.currently.temperature, data.currently.icon);
+    }).fail(function(xhr, textStatus, errorThrown) {
+        if (xhr.status == 403) {
+            _gaq.push(['_trackEvent', 'Key Failed', randomApiKey]);
+            if (getWeatherRetry < 3) {
+                getWeatherRetry++;
+                setTimeout(function() {
+                    getLocation();
+                }, 1000);
+            } else {
+                _gaq.push(['_trackEvent', 'Failed Multiple Times', 'Last Key: ' + randomApiKey]);
+                alert('Something went wrong. Please try again.')
+            }
         }
     });
 };
 
-var updateBadge = function(temp) {
+var updateBadge = function(temp, icon) {
     chrome.browserAction.setBadgeText({
         text: convertTemp(temp).toString() //+ '°'
     });
+    chrome.browserAction.setIcon({
+        path: 'img/badge/' + badgeIcons[icon]
+    })
 };
 
 var displayForcast = function(daily) {
-    for (i = 1; i <= 5; i++) {
+    for (i = 0; i <= 5; i++) {
         var daySection = $('#day' + i);
+        daySection.tooltipster('content', buildToolTipDetail(daily.data[i], false));
         daySection.find('.dow').text(moment.unix(daily.data[i].time).format('ddd'));
         daySection.find('.high').html(convertTemp(daily.data[i].temperatureMax) + '&deg;');
         daySection.find('.low').html(convertTemp(daily.data[i].temperatureMin) + '&deg;');
@@ -133,6 +158,7 @@ var displayHourly = function(hourly) {
 
     for (i = 1; i <= 8; i++) {
         var hourSection = $('#hour' + i);
+        hourSection.tooltipster('content', buildToolTipDetail(hourly.data[i], true));
         hourSection.find('.time').text(moment.unix(hourly.data[i].time).format(timeFormat));
         hourSection.find('.temp').html(convertTemp(hourly.data[i].temperature) + '&#176;');
         hourSection.find('.icon').html(weatherIcons[hourly.data[i].icon]);
@@ -140,16 +166,36 @@ var displayHourly = function(hourly) {
     }
 };
 
+var buildToolTipDetail = function(data, showFeelsLike){
+    return '<div class="toolTipDetail">'
+    +'<dl>'
+        +(showFeelsLike ? '<dt>Feels like:</dt>' : '')
+        +(showFeelsLike ?'<dd>'+(data.apparentTemperature ? convertTemp(data.apparentTemperature) + '&#176;' : '--' )+'</dd>' : '')
+        +'<dt>Wind:</dt>'
+        +'<dd>'+(data.windSpeed ? Math.round(data.windSpeed) + ' mph' : '--' )+'</dd>'
+        +'<dt>Humidity:</dt>'
+        +'<dd>'+(data.humidity ? Math.round(data.humidity * 100) + '%' : '--' )+'</dd>'
+        +'<dt>Dew Pt:</dt>'
+        +'<dd>'+(data.dewPoint ? Math.round(data.dewPoint) : '--' )+'</dd>'
+        +'<dt>Visibility:</dt>'
+        +'<dd>'+(data.visibility ? Math.round(data.visibility) + ' mi' : '--' )+'</dd>'
+        +'<dt>Pressure:</dt>'
+        +'<dd>'+(data.pressure ? Math.round(data.pressure) + ' mb' : '--' )+'</dd>'
+    +'</dl></div>';
+};
+
 var displayCurrent = function(currently) {
     var currentSection = $('.current');
-    currentSection.find('.summary').text(currently.summary);
+    currentSection.find('.temperature').tooltipster('content', buildToolTipDetail(currently, true));
+    currentSection.find('.summary').html(currently.summary);
     currentSection.find('.right .currentIcon').html(weatherIcons[currently.icon]);
     currentSection.find('.temperature').html(convertTemp(currently.temperature) + '&#176;');
+    currentSection.find('.feelsLike').html('Feels like ' + convertTemp(currently.apparentTemperature) + '&#176;');
 };
 
 var displayAlerts = function(alerts) {
+    $('#alerts').html('');
     if (alerts) {
-        $('#alerts').html('');
         alerts.forEach(function(alert) {
             $('#alerts').append('<li><a target="_blank" href="' + alert.uri + '">' + alert.title + '</a></li>')
         });
@@ -218,7 +264,7 @@ var bindActions = function() {
         getLocation();
     });
 
-    $('.summary, .currentIcon, .temperature, .hourly, .forecast').click(function(){
+    $('.currentIcon, .temperature, .hourly, .forecast').click(function() {
         var latitude = localStorage.getItem('latitude');
         var longitude = localStorage.getItem('longitude');
         window.open('http://forecast.io/#/f/' + latitude + ',' + longitude);
@@ -253,8 +299,11 @@ var convertTemp = function(tempValue) {
 var installUpdate = function() {
     chrome.runtime.onInstalled.addListener(function(details) {
         if (details.reason == "install") {
-             window.open('http://timleland.com/weather-chrome-extension/');
+            getLocation();
+            window.open('http://timleland.com/weather-chrome-extension/');
         } else if (details.reason == "update") {
+            getLocation();
+            window.open('http://timleland.com/weather-chrome-extension/#update');
             var thisVersion = chrome.runtime.getManifest().version;
             console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
         }
