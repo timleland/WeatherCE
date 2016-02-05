910f22fe69acea0ba48ef8f7aed8e39b8f85919b
Tim Leland <timothyl88@gmail.com>
2016-02-04 22:02:48 -0500

    3.5.5

--- app/js/popup.js
+++ app/js/popup.js
@@ -174,7 +174,7 @@ APP.popup = function() {
 
     var getWeather = function() {
         $('#weather_embed').attr('src', apiurl + 'embed/' + _appId + '/sort/0');
-        setTimeout(function(){ updateBadge(_appId, false); }, 4000);
+        setTimeout(function(){ updateBadge(_appId, false); }, 2500);
     };
 
     var getAppId = function(callBack, fromBackground) {
