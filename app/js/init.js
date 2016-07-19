40165c30198173584b192b15a92e70b74c9258fd
Tim Leland <timothyl88@gmail.com>
2016-07-19 08:55:57 -0400

    Adding videos

--- app/js/init.js
+++ app/js/init.js
@@ -1,6 +1,6 @@
 //var baseUrl = 'http://weathertest.timleland.com/';
 var baseUrl = 'https://weather.timleland.com/';
-var _extensionType = 'Opera';
+var _extensionType = 'Chrome';
 var apiurl = baseUrl + 'api/v1/';
 var APP = {};
 
